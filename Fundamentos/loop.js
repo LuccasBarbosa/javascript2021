8c4a7623ca2f380a6b5588d6a54a5e8f8977eff2
Luccas Barbosa <eu@luccasbarbosa.com.br>
2021-06-21 18:10:12 -0300

    Fundamentos

--- Fundamentos/loop.js
+++ Fundamentos/loop.js
@@ -9,3 +9,30 @@ for(var i= 0; i <= lista.length; i++){
 for (let a = 0; a <= 10; a++){
     console.log(a);
 }
+
+
+const funcs = []
+
+for (var index = 0; index < 10; index++) {
+    funcs.push(function(){
+        console.log(index);
+    })
+    
+}
+
+funcs[2]() /* retorna 10 */
+funcs[8]() /* retorna 10 */
+
+
+
+funcs2 = []
+
+for (let t = 0; t < 10; t++) {
+    funcs2.push(function(){
+        console.log(t);
+    })
+    
+}
+
+funcs2[2]() /* retorna 2 */
+funcs2[8]() /* retorna 8 */
