2a897cc57fb97c2f09fbf6e36f0aa038b5f41237
Lucas Barbosa <eu@luccasbarbosa.com.br>
2021-07-04 14:28:33 -0300

    Aritmeticos

--- Fundamentos/aritimeticos.js
+++ Fundamentos/aritimeticos.js
@@ -1,5 +1,9 @@
 const [a,b,c,d] = [1,2,3,4]
 
 let soma = a + b + c + d
+let subtracao = a - b
+let multiplicacao = a * b 
+let divisao = a / b 
+let modulo = a % b 
 
-console.log(soma);
+console.log(soma, subtracao, multiplicacao, divisao, modulo);
