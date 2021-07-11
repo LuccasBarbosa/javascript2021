d0de3c90473c99c3f4028fedb98f7abbd4e5905d
Lucas Barbosa <eu@luccasbarbosa.com.br>
2021-07-10 21:26:54 -0300

    Tipos Declaracoes

---
+++ Funcoes/tiposDeclaracoes.js
@@ -0,0 +1,20 @@
+console.log(soma(2,2))
+
+// function declaration
+function soma(a,b){
+    return a + b
+}
+
+
+// function expression
+const sub = function(c,d){
+    return c - d
+}
+
+// named function expression
+var mult = function mult(e,f){
+    return e * f
+}
+
+console.log(sub(10,8))
+console.log(mult(19,19))
