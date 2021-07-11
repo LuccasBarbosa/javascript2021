77ba7b0890fd41180b17b94590d96d851f8abd85
Lucas Barbosa <eu@luccasbarbosa.com.br>
2021-07-10 23:39:26 -0300

    Diferenças

--- Funcoes/classeFactoryConstrutora.js
+++ Funcoes/classeFactoryConstrutora.js
@@ -8,7 +8,7 @@ class Pessoa{
     falar = nome => { console.log(`Oi meu nome é ${this.nome}`);}
 }
 
-p1 = new Pessoa('Lucas')
+p1 = new Pessoa('Fulano')
 p1.falar()
 
 
@@ -23,7 +23,7 @@ const criarPessoa = function(nome){
 
 }
 
-p2 = new criarPessoa('Silvia')
+p2 = new criarPessoa('Ciclano')
 p2.falar()
 
 
@@ -31,9 +31,10 @@ p2.falar()
 function novaPessoa(nome){
     this.nome = nome
     this.falar = function (){
-        console.log(`Oi meu nome é ${nome}`)
+        console.log(`Oi meu nome é ${this.nome}`)
     }
 }
 
-p3 = new novaPessoa('Nicolas')
+p3 = new novaPessoa('Beltrano')
 p3.falar()
+console.log(p3.nome); 
